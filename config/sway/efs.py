999edc9ad0036f76a9265679f413c61eddd777ac
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-04-03 16:08:59 -0400

    Update efs.py

--- config/sway/efs.py
+++ config/sway/efs.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 
 subprocess.run(["echo 'dev' | sudo -S mv /" + os.getenv('TEAM_NAME') + " /home/dev"], shell=True)
-subprocess.run(["echo 'dev' | sudo -S chmod ugo+rwx /home/dev/' + os.getenv('TEAM_NAME') + "/" + os.getenv('WORKSPACE_NAME')"], shell=True)
+subprocess.run(["echo 'dev' | sudo -S chmod ugo+rwx /home/dev/" + os.getenv('TEAM_NAME') + "/" + os.getenv('WORKSPACE_NAME')], shell=True)
