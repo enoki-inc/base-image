d0bfc55178a3de2486a3ee6fed8f15421aa65214
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-04-05 16:39:53 -0400

    update to save_terminal_history.py

--- config/sway/save_terminal_history.py
+++ config/sway/save_terminal_history.py
@@ -3,16 +3,20 @@ import subprocess
 
 team_name = os.getenv('TEAM_NAME')
 workspace_name = os.getenv('WORKSPACE_NAME')
-filepath = f'~/{team_name}/{workspace_name}/tmp/history.txt'
-command = f"cp ~/.bash_history {filepath}"
+filepath = f'home/dev/{team_name}/{workspace_name}/tmp/history.txt'
+command = f"cp home/dev/.bash_history {filepath}"
 
-if not os.path.exists(os.path.dirname(filepath)):
-    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+directory = os.path.dirname(os.path.expanduser(filepath))
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-# Check if ~/.bash_history is empty
-if os.stat(os.path.expanduser('~/.bash_history')).st_size == 0:
-    # If empty, write "test" to it
-    subprocess.run(["echo test > ~/.bash_history"], shell=True)
+if not os.path.exists(os.path.expanduser(filepath)):
+    # Create an empty file if it doesn't exist
+    subprocess.run(["touch", os.path.expanduser(filepath)], check=True)
 
+# Append the existing terminal commands to ~/.bash_history
+subprocess.run(["bash", "-c", "history", "-a"], shell=True)
 # Copy the history file to the new location
 subprocess.run([command], shell=True)
+
+print("Terminal history saved!")
