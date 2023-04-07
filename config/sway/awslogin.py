90d6c5ba6862ed0a9586fd5b0a96c30fb7d36cee
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-04-07 16:46:07 -0400

    Update awslogin.py

--- config/sway/awslogin.py
+++ config/sway/awslogin.py
@@ -9,13 +9,15 @@ if os.getenv('AWS_ACCESS_KEY') != "":
     # Set AWS CLI credentials and output format
     subprocess.run(["aws", "configure", "set", "aws_access_key_id", access_key], check=True)
     subprocess.run(["aws", "configure", "set", "aws_secret_access_key", secret_key], check=True)
-    subprocess.run(["aws", "configure", "set", "region", region], check=True)
+    subprocess.run(["aws", "configure", "set", "region", "us-east-1"], check=True)
     subprocess.run(["aws", "configure", "set", "output", "json"], check=True)
 
     # Authenticate to CodeCommit
-    subprocess.run(["aws", "codecommit", "credential-helper", "approve"], check=True)
+    subprocess.run(["git", "config", "--global", "credential.helper",'!aws codecommit credential-helper $@'])
 
-    print("Successfully logged in to AWS Code Commit in Enoki Workspace!")
+    subprocess.run(["git", "config", "--global", "credential.UseHttpPath", "true"])
+
+    print("Successfully configured AWS in Enoki Workspace!")
 
 else:
     print("AWS_ACCESS_KEY not set. Please set the AWS_ACCESS_KEY environment variable to log into AWS Code Commit.")
