48b1f0e115c2db9a6dce60dc98f6f8dd7979b008
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-05-09 19:12:17 -0400

    Create local_ssh.py

---
+++ config/sway/local_ssh.py
@@ -0,0 +1,13 @@
+import os
+
+# Get the environment variables
+ssh_user = os.getenv('SSH_USER')
+ssh_hostname = os.getenv('SSH_HOSTNAME')
+ssh_port = os.getenv('SSH_PORT')
+
+if ssh_user != "":
+  # Define the folder URI for the remote-ssh connection
+  folder_uri = f'vscode-remote://ssh-remote+{ssh_user}@{ssh_hostname}:{ssh_port}/'
+
+  # Execute the command to open Visual Studio Code with the remote-ssh extension
+  os.system(f'code --no-sandbox --folder-uri "{folder_uri}"')
